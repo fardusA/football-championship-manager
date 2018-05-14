2d9a48ade5a593a786ba571513f6265bd9e975f8
Fardus Abdirahman <fardus.abdirahman@my.westminster.ac.uk>
2018-05-14 17:24:41 +0100

    Create MainClass.java
    the main class which will run the system

---
+++ src/MainClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package footballchampionships;
+
+/**
+ *
+ * @author Main User
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        PremierLeagueManager manager = new PremierLeagueManager(); //make a new object of class PremierLeagueManager
+        manager.menuoption(); //use object to run methods inside premierLeagueManager, this runs the method menuoption
+        
+    }
+    
+}
